cfc8dccafd9b005de6d7fa3772390a1f016e0c47
Petyo156 <petyo156bg@gmail.com>
2024-08-09 11:32:16 +0300

    Spring Boot Admin implemented(works with hotel, comments and bff)

--- rest/src/main/java/com/tinqinacademy/admin/rest/Main.java
+++ rest/src/main/java/com/tinqinacademy/admin/rest/Main.java
@@ -1,14 +1,15 @@
 package com.tinqinacademy.admin.rest;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.tinqinacademy.admin")
+@EnableAdminServer
 public class Main {
     public static void main(String[] args) {
-        //run
         SpringApplication.run(Main.class, args);
     }
 }
